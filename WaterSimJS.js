914b670130682a0dfc4fb67cd396d2a2da565648
ectoplasm33 <ectoplasm128@gmail.com>
2025-12-20 23:23:47 -0600

    Add files via upload

--- WaterSimJS.js
+++ WaterSimJS.js
@@ -1,7 +1,10 @@
 const canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-const particle_radius = 3.0 / canvas.width;
+const particle_radius = 100.0 / canvas.width;
+
+const canvas_x = canvas.width * 0.5;
+const canvas_y = canvas.height * 0.5;
 
 let num_particles = 1000;
 let max_particles = 10000;
@@ -18,6 +21,7 @@ let inv_rep_r = 1.0 / repulsion_radius;
 let inv_h = 1.0 / influence_radius;
 let cell_size = influence_radius;
 let inv_cs = 1.0 / cell_size;
+const grid_hash = canvas.width + 1;
 
 const target_fps = 120;
 const delay = 1000.0 / target_fps;
@@ -38,20 +42,51 @@ let show_fps = false;
 
 let active = true;
 
-const num_vars = 5;
+const grid = new Map();
+
+const num_vars = 7;
+const render_vars = 3
 
 const particle_data = new Float32Array(max_particles * num_vars);
-const normalized_particles = new Float32Array(max_particles * num_vars)
+const new_particles = new Float32Array(max_particles * num_vars);
+const normalized_particles = new Float32Array(max_particles * render_vars)
+
+const max_neighbors = 150;
+const particle_neighbors = new Array(max_particles);
+const particle_neighbor_count = new Int16Array(max_neighbors);
+
+const neighbor_info = new Array(max_particles);
+
+const calculated_values = new Array(max_particles);
+
+const particle_keys = new Int32Array(max_particles);
 
 for (let i = 0; i < num_particles; i++) {
     let j = i*num_vars;
     particle_data[j] = (Math.random()-0.5) * canvas.width;
     particle_data[j+1] = (Math.random()-0.5) * canvas.height;
-    particle_data[j+2] = particle_radius;
+    particle_data[j+2] = 0;
     particle_data[j+3] = 0;
     particle_data[j+4] = 0;
+    particle_data[j+5] = 0;
+    particle_data[j+6] = 0;
 
-    normalized_particles[j+2] = particle_radius;
+    for (let k = 0; k < num_vars; k++) {
+        new_particles[j+k] = 0;
+    }
+
+    normalized_particles[i*render_vars+2] = particle_radius;
+}
+
+for (let i = 0; i < max_particles; i++) {
+    particle_neighbors[i] = new Int16Array(max_neighbors);
+    neighbor_info[i] = new Array(max_neighbors);
+    calculated_values[i] = new Float32Array(4)
+    particle_neighbor_count[i] = 0;
+
+    for (let j = 0; j < max_neighbors; j++) {
+        neighbor_info[i][j] = new Float32Array(6);
+    }
 }
 
 const quad_vertices = new Float32Array([
@@ -214,7 +249,7 @@ const pipeline = device.createRenderPipeline({
                 ]
             },
             { // instance buffer
-                arrayStride: num_vars * 4,
+                arrayStride: render_vars * 4,
                 stepMode: 'instance',
                 attributes: [
                     { shaderLocation: 1, offset: 0, format: 'float32x2' }, // x,y
@@ -247,15 +282,245 @@ const pipeline = device.createRenderPipeline({
 
 // main loop
 
-const inv_cw = 1.0 / canvas.width;
-const inv_ch = 1.0 / canvas.height;
+const inv_cw = 2.0 / canvas.width;
+const inv_ch = 2.0 / canvas.height;
 
 async function main_loop() {
 
+    grid.clear();
     for (let i = 0; i < num_particles; i++) {
         let j = i*num_vars;
-        normalized_particles[j] = particle_data[j] * inv_cw * 2.0;
-        normalized_particles[j+1] = particle_data[j+1] * inv_ch * 2.0;
+        
+        let cx = Math.floor(particle_data[j] * inv_cs);
+        let cy = Math.floor(particle_data[j+1] * inv_cs);
+
+        let key = cy * grid_hash + cx;
+
+        if (!grid.has(key)) {
+            grid.set(key, []);
+        }
+
+        grid[key].push(i);
+
+        particle_keys[i] = key;
+    }
+
+    for (let i = 0; i < num_particles; i++) {
+        particle_neighbor_count[i] = 0;
+
+        let k = particle_keys[i];
+
+        for (let dy = -1; dy < 2; dy++) {
+            let row_key = dy * grid_hash;
+
+            for (let dx = -1; dx < 2; dx++) {
+                const key = k + dx + row_key;
+
+                const cell = grid[key]
+
+                let j = 0;
+                for (j; j < cell.length; j++) {
+                    particle_neighbors[i][j] = cell[j]
+                }
+
+                particle_neighbor_count[i] = j;
+            }
+        }
+
+        const count = particle_neighbor_count[i];
+        
+        let density = 0.0;
+
+        const p = i*num_vars;
+
+        for (let j = 0; j < count; j++) {
+            const n = particle_neighbors[i][j]*num_vars;
+            
+            let dx = particle_data[n] - particle_data[p+4];
+            let dy = particle_data[n+1] - particle_data[p+5];
+            let dist = dx*dx + dy*dy;
+
+            if (dist < h_sq) {
+                neighbor_info[i][j][0] = dx;
+                neighbor_info[i][j][1] = dy;
+                dist = Math.sqrt(dist);
+                neighbor_info[i][j][2] = dist;
+
+                let q = (influence_radius - dist) * inv_h;
+                let q2 = q*q;
+                let q3 = q2*q;
+
+                neighbor_info[i][j][3] = q;
+                neighbor_info[i][j][4] = q2;
+                neighbor_info[i][j][5] = q3;
+
+                density += q3;
+            }
+        }
+
+        particle_data[p+6] = Math.max(density, 1e-6);
+    }
+
+    for (let i = 0; i < num_particles; i++) {
+        const p = i*num_vars;
+        
+        let fx = 0.0;
+        let fy = 0.0;
+
+        let nx = 0.0;
+        let ny = 0.0;
+
+        const pressure_i = (particle_data[p+6] - target_density) * pressure_multiplier;
+
+        const count = particle_neighbor_count[i];
+
+        for (let j = 0; j < count; j++) {
+            const n = particle_neighbors[i][j]*num_vars;
+
+            const neighbor = neighbor_info[i][j];
+
+            const dist = neighbor[2];
+            let inv_dst;
+
+            if (dist == 0) {
+                inv_dst = 0;
+            } else {
+                inv_dst = 1.0 / dist;
+            }
+
+            const n_x = neighbor[0] * inv_dst;
+            const n_y = neighbor[1] * inv_dst;
+
+            const n_density = particle_data[n+6]; 
+
+            const pressure_j = (n_density - target_density) * pressure_multiplier;
+
+            const inv_nd = 1.0 / n_density;
+
+            let q2 = neighbor[4];
+
+            const f = (pressure_i + pressure_j) * 0.5 * q2;
+
+            fx -= f * n_x;
+            fy -= f * n_y;
+
+            let q = q2 * viscosity_multipler * inv_nd;
+
+            fx += (particle_data[n+2] - particle_data[p+2]) * q;
+            fy += (particle_data[n+3] - particle_data[p+3]) * q;
+
+            nx += n_x * inv_nd;
+            ny += n_y * inv_nd;
+
+            if (dist < repulsion_radius) {
+                q = (repulsion_radius - dist) * inv_rep_r;
+                q2 = q*q;
+                fx -= n_x * q2;
+                fy -= n_y * q2;
+            }
+        }
+
+        calculated_values[i][0] = fx;
+        calculated_values[i][1] = fy;
+
+        if (nx != 0 || ny != 0) {
+            const inv_m = 1.0 / Math.sqrt(nx*nx + ny*ny);
+
+            calculated_values[i][2] = nx * inv_m;
+            calculated_values[i][3] = ny * inv_m;
+        } else {
+            calculated_values[i][2] = 0;
+            calculated_values[i][3] = 0;
+        }
+    }
+
+    for (let i = 0; i < num_particles; i++) {
+        const p = i*num_vars;
+
+        const values = calculated_values[i];
+
+        let fx = values[0];
+        let fy = values[1];
+
+        const nx = values[2];
+        const ny = values[3];
+
+        let k = 0.0;
+
+        let mag = nx*nx + ny*ny;
+
+        if (mag > 0.01) {
+            const size = particle_neighbor_count[i];
+
+            for (let j = 0; j < size; j++) {
+                const n = particle_neighbors[i][j]
+                const neighbor_vals = calculated_values[n];
+                const neighbor = neighbor_info[i][j];
+
+                k -= ((neighbor_vals[2] - nx) * neighbor[0] + (neighbor_vals[3] - ny) * neighbor[1]) * neighbor[5] / (neighbor[2] * particle_data[n*num_vars + 6]);
+            }
+
+            k = Math.min(Math.max(k, -inv_rep_r), inv_rep_r);
+
+            let q = surface_tension_mp * k;
+
+            fx += nx * q;
+            fy += ny * q;
+        }
+
+        let vx = particle_data[p+2] + fx;
+        let vy = particle_data[p+3] + fy + gravity;
+
+        const m2 = vx*vx + vy*vy;
+
+        if (m2 > 1225) {
+            const m1 = 1.0 / Math.sqrt(m2);
+
+            vx *= m1;
+            vy *= m1;
+        }
+
+        if (render_colors) {
+            const m = Math.sqrt(m2);
+            const m3 = m2 * m;
+            //add color based on velocity
+        }
+
+        let x = particle_data[p] + particle_data[p+2];
+        let y = particle_data[p+1] + particle_data[p+3];
+
+        if (x < -canvas_x) {
+            x = -canvas_x;
+            vx *= -0.5;
+        } else if (x > canvas_x) {
+            x = canvas_x;
+            vx *= -0.5;
+        }
+        if (y < -canvas_y) {
+            y = -canvas_y;
+            vy *= -0.5;
+        } else if (y > canvas_y) {
+            y = canvas_y;
+            vy *= -0.5;
+        }
+
+        new_particles[p+2] = vx;
+        new_particles[p+3] = vy;
+
+        new_particles[p] = x;
+        new_particles[p+1] = y;
+
+        new_particles[p+4] = x + vx;
+        new_particles[p+5] = y + vy;
+
+        // add mouse interaction
+    }
+
+    for (let i = 0; i < num_particles; i++) {
+        let j1 = i*num_vars;
+        let j2 = i*render_vars;
+        normalized_particles[j2] = particle_data[j1] * inv_cw;
+        normalized_particles[j2+1] = particle_data[j1+1] * inv_ch;
     }
 
     device.queue.writeBuffer(instance_buffer, 0, normalized_particles);
