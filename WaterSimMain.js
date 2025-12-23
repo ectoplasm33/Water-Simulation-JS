02c9f422c7cff29318a577556438d19b0880489c
ectoplasm33 <ectoplasm128@gmail.com>
2025-12-23 10:34:56 -0600

    Add files via upload

---
+++ WaterSimMain.js
@@ -0,0 +1,552 @@
+let num_particles = 3800;
+let max_particles = 12000;
+let influence_radius = 48.0;
+let target_density  = 6.5;
+let pressure_multiplier = 0.16;
+let viscosity_multipler = 0.13;
+let surface_tension_mp = 1.0;
+let gravity = -0.25;
+
+let h_sq = influence_radius * influence_radius;
+let repulsion_radius = 0.5 * influence_radius;
+let inv_rep_r = 1.0 / repulsion_radius;
+let inv_h = 1.0 / influence_radius;
+let cell_size = influence_radius;
+let inv_cs = 1.0 / cell_size;
+
+const target_fps = 120;
+const delay = 1000.0 / target_fps;
+
+let mouse_strength = .6;
+let mouse_influence_r = 250.0;
+let mouse_r2 = mouse_influence_r * mouse_influence_r;
+
+let lmb = false;
+let rmb = false;
+
+let mouse_x = -1e5;
+let mouse_y = -1e5;
+
+let render_colors = false;
+let show_ui = false;
+let show_fps = false;
+
+let active = true;
+
+const grid = new Map();
+
+const num_vars = 7;
+const render_vars = 3
+
+const particle_data = new Float32Array(max_particles * num_vars);
+const new_particles = new Float32Array(max_particles * num_vars);
+const normalized_particles = new Float32Array(max_particles * render_vars)
+
+const max_neighbors = 150;
+const particle_neighbors = new Array(max_particles);
+const particle_neighbor_count = new Int16Array(max_particles);
+
+const neighbor_info = new Array(max_particles);
+
+const calculated_values = new Array(max_particles);
+
+const particle_keys = new Int32Array(max_particles);
+
+const canvas = document.querySelector('canvas');
+canvas.x = 0;
+canvas.y = 0;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const particle_radius = 3.0 / canvas.width;
+
+for (let i = 0; i < num_particles; i++) {
+    let j = i*num_vars;
+    particle_data[j] = (Math.random()-0.5) * canvas.width;
+    particle_data[j+1] = (Math.random()-0.5) * canvas.height;
+    particle_data[j+2] = 0;
+    particle_data[j+3] = 0;
+    particle_data[j+4] = 0;
+    particle_data[j+5] = 0;
+    particle_data[j+6] = 0;
+
+    for (let k = 0; k < num_vars; k++) {
+        new_particles[j+k] = 0;
+    }
+
+    normalized_particles[i*render_vars+2] = particle_radius;
+}
+
+for (let i = 0; i < max_particles; i++) {
+    particle_neighbors[i] = new Int32Array(max_neighbors);
+    neighbor_info[i] = new Array(max_neighbors);
+    calculated_values[i] = new Float32Array(4)
+    particle_neighbor_count[i] = 0;
+
+    for (let j = 0; j < max_neighbors; j++) {
+        neighbor_info[i][j] = new Float32Array(6);
+    }
+}
+
+const canvas_x = canvas.width * 0.5;
+const canvas_y = canvas.height * 0.5;
+
+const grid_hash = canvas.width + 1;
+
+const ratio = canvas.width / canvas.height;
+
+const quad_vertices = new Float32Array([
+  -1, -ratio,
+   1, -ratio,
+  -1,  ratio,
+   1,  ratio,
+]);
+
+if (!navigator.gpu) {
+    throw new Error("WebGPU not supported on this browser.");
+}
+
+const adapter = await navigator.gpu.requestAdapter();
+if (!adapter) {
+    throw new Error("No appropriate GPUAdapter found.");
+}
+
+const device = await adapter.requestDevice();
+
+const context = canvas.getContext('webgpu');
+const canvas_format = navigator.gpu.getPreferredCanvasFormat();
+
+context.configure({
+    device: device,
+    format: canvas_format,
+    alphaMode: 'premultiplied',
+});
+
+const quad_buffer = device.createBuffer({
+  size: quad_vertices.byteLength,
+  usage: GPUBufferUsage.VERTEX,
+  mappedAtCreation: true
+});
+
+new Float32Array(quad_buffer.getMappedRange()).set(quad_vertices);
+quad_buffer.unmap();
+
+const instance_buffer = device.createBuffer({
+    size: particle_data.byteLength,
+    usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+});
+
+const uniform_data = new Float32Array([canvas.width, canvas.height]);
+
+const vertex_shader_code = `
+struct vertexOutput {
+    @builtin(position) position : vec4<f32>,
+    @location(0) localPos : vec2<f32>,
+    @location(1) radius : f32
+};
+
+@vertex
+fn vs_main(
+    @location(0) quadPos : vec2<f32>,
+    @location(1) center : vec2<f32>,
+    @location(2) radius : f32
+) -> vertexOutput {
+    var out : vertexOutput;
+
+    out.position = vec4<f32>(
+        center + quadPos * radius,
+        0.0,
+        1.0
+    );
+
+    out.localPos = quadPos;
+    out.radius = radius;
+
+    return out;
+}
+`;
+
+const fragment_shader_code = `
+@fragment
+fn fs_main(
+    @builtin(position) position : vec4<f32>,
+    @location(0) center : vec2<f32>,
+    @location(1) radius : f32
+) -> @location(0) vec4<f32> {
+
+    // let dist = distance(position.xy, center);
+
+    // if (dist > 0.5) {
+    //     discard;
+    // }
+
+    return vec4<f32>(0, 0.156862745098, 0.941176470588, 0.666666666667);
+}
+`;
+
+const vertex_module = device.createShaderModule({
+    code: vertex_shader_code
+});
+
+const fragment_module = device.createShaderModule({
+    code: fragment_shader_code
+});
+
+const pipeline = device.createRenderPipeline({
+    vertex: {
+        module: vertex_module,
+        entryPoint: 'vs_main',
+        buffers: [
+            { // quad vertex buffer
+                arrayStride: 2 * 4,
+                attributes: [
+                    { shaderLocation: 0, offset: 0, format: 'float32x2' }
+                ]
+            },
+            { // instance buffer
+                arrayStride: render_vars * 4,
+                stepMode: 'instance',
+                attributes: [
+                    { shaderLocation: 1, offset: 0, format: 'float32x2' }, // x,y
+                    { shaderLocation: 2, offset: 2 * 4, format: 'float32' } // radius
+                ]
+            }
+        ]
+    },
+    fragment: {
+        module: fragment_module,
+        entryPoint: 'fs_main',
+        targets: [{ format: canvas_format }]
+    },
+    primitive: {
+        topology: 'triangle-strip'
+    },
+    layout: 'auto'
+});
+
+const inv_cw = 2.0 / canvas.width;
+const inv_ch = 2.0 / canvas.height;
+
+canvas.addEventListener("mousemove", e => {
+    const rect = canvas.getBoundingClientRect();
+    mouse_x = e.clientX - rect.left - canvas_x;
+    mouse_y = rect.top - e.clientY + canvas_y;
+});
+
+canvas.addEventListener("mousedown", e => {
+    if (e.button == 0) lmb = true;
+    if (e.button == 2) rmb = true;
+});
+
+canvas.addEventListener("mouseup", e => {
+    if (e.button == 0) lmb = false;
+    if (e.button == 2) rmb = false;
+});
+
+canvas.addEventListener('contextmenu', function(event) {
+    event.preventDefault(); 
+});
+
+async function main_loop() {
+
+    grid.clear();
+    for (let i = 0; i < num_particles; i++) {
+        const j = i*num_vars;
+        
+        const cx = 1e6 + Math.floor(particle_data[j+4] * inv_cs);
+        const cy = 1e6 + Math.floor(particle_data[j+5] * inv_cs);
+
+        const key = cy * grid_hash + cx;
+
+        if (!grid.has(key)) {
+            grid.set(key, []);
+        }
+
+        grid.get(key).push(i);
+
+        particle_keys[i] = key;
+    }
+
+    for (let i = 0; i < num_particles; i++) {
+        const p = i*num_vars;
+        const k = particle_keys[i];
+
+        let num = 0;
+        let density = 0.0;
+        
+        let brk = false;
+        for (let dy = -1; dy < 2; dy++) {
+            const row_key = dy * grid_hash;
+
+            for (let dx = -1; dx < 2; dx++) {
+                const key = k + dx + row_key;
+                
+                if (!grid.has(key)) continue;
+
+                const cell = grid.get(key);
+
+                for (let j = 0; j < cell.length; j++) {
+                    if (cell[j] == i) continue;
+                    
+                    const n = cell[j]*num_vars;
+
+                    const dx = particle_data[n+4] - particle_data[p+4];
+                    const dy = particle_data[n+5] - particle_data[p+5];
+                    let dist = dx*dx + dy*dy;
+
+                    if (dist < h_sq) {
+                        particle_neighbors[i][num] = cell[j];
+                        neighbor_info[i][num][0] = dx;
+                        neighbor_info[i][num][1] = dy;
+                        dist = Math.sqrt(dist);
+                        neighbor_info[i][num][2] = dist;
+
+                        let q = (influence_radius - dist) * inv_h;
+                        let q2 = q*q;
+                        let q3 = q2*q;
+
+                        neighbor_info[i][num][3] = q;
+                        neighbor_info[i][num][4] = q2;
+                        neighbor_info[i][num][5] = q3;
+
+                        density += q3;
+
+                        num++;
+                    }  
+
+                    if (num == max_neighbors) { 
+                        brk = true;
+                        break;
+                    }
+                }   
+                if (brk) break;
+            }  
+            if (brk) break;  
+        }
+
+        particle_data[p+6] = Math.max(density, 1e-6);
+
+        particle_neighbor_count[i] = num;
+    }
+
+    for (let i = 0; i < num_particles; i++) {
+        const p = i*num_vars;
+        
+        let fx = 0.0;
+        let fy = 0.0;
+
+        let nx = 0.0;
+        let ny = 0.0;
+
+        const pressure_i = (particle_data[p+6] - target_density) * pressure_multiplier;
+
+        const count = particle_neighbor_count[i];
+
+        for (let j = 0; j < count; j++) {
+            const n = particle_neighbors[i][j]*num_vars;
+
+            const neighbor = neighbor_info[i][j];
+
+            const dist = neighbor[2];
+            let inv_dst;
+
+            if (dist == 0) {
+                inv_dst = 0;
+            } else {
+                inv_dst = 1.0 / dist;
+            }
+
+            const n_x = neighbor[0] * inv_dst;
+            const n_y = neighbor[1] * inv_dst;
+
+            const n_density = particle_data[n+6]; 
+
+            const pressure_j = (n_density - target_density) * pressure_multiplier;
+
+            let inv_nd = 0.0;
+            if (n_density > 0) {
+                inv_nd = 1.0 / n_density;
+            }
+
+            let q2 = neighbor[4];
+
+            const f = (pressure_i + pressure_j) * 0.5 * q2;
+
+            fx -= f * n_x;
+            fy -= f * n_y;
+
+            let q = q2 * viscosity_multipler * inv_nd;
+
+            fx += (particle_data[n+2] - particle_data[p+2]) * q;
+            fy += (particle_data[n+3] - particle_data[p+3]) * q;
+
+            nx += n_x * inv_nd;
+            ny += n_y * inv_nd;
+
+            if (dist < repulsion_radius) {
+                q = (repulsion_radius - dist) * inv_rep_r;
+                q2 = q*q;
+                fx -= n_x * q2;
+                fy -= n_y * q2;
+            }
+        }
+
+        calculated_values[i][0] = fx;
+        calculated_values[i][1] = fy;
+
+        if (nx != 0 || ny != 0) {
+            const inv_m = 1.0 / Math.sqrt(nx*nx + ny*ny);
+
+            calculated_values[i][2] = nx * inv_m;
+            calculated_values[i][3] = ny * inv_m;
+        } else {
+            calculated_values[i][2] = 0;
+            calculated_values[i][3] = 0;
+        }
+    }
+
+    for (let i = 0; i < num_particles; i++) {
+        const p = i*num_vars;
+
+        const values = calculated_values[i];
+
+        let fx = values[0];
+        let fy = values[1];
+
+        const nx = values[2];
+        const ny = values[3];
+
+        let mag = nx*nx + ny*ny;
+
+        if (mag > 0.01) {
+            let k = 0.0;
+            const size = particle_neighbor_count[i];
+
+            for (let j = 0; j < size; j++) {
+                const n = particle_neighbors[i][j]
+                const neighbor_vals = calculated_values[n];
+                const neighbor = neighbor_info[i][j];
+
+                let d = neighbor[2] * particle_data[n*num_vars + 6];
+
+                if (d == 0) {
+                    d = 1;
+                }
+
+                k -= ((neighbor_vals[2] - nx) * neighbor[0] + (neighbor_vals[3] - ny) * neighbor[1]) * neighbor[5] / d;
+            }
+
+            k = Math.min(Math.max(k, -inv_rep_r), inv_rep_r);
+
+            const q = surface_tension_mp * k;
+
+            fx += nx * q;
+            fy += ny * q;
+        }
+
+        let vx = particle_data[p+2] + fx;
+        let vy = particle_data[p+3] + fy + gravity;
+
+        const m2 = vx*vx + vy*vy;
+
+        if (m2 > 1225) {
+            const m1 = 35.0 / Math.sqrt(m2);
+
+            vx *= m1;
+            vy *= m1;
+        }
+        
+        if (render_colors) {
+            const m = Math.sqrt(m2);
+            const m3 = m2 * m;
+            //add color based on velocity
+        }
+
+        let x = particle_data[p] + vx;
+        let y = particle_data[p+1] + vy;
+
+        if (x < -canvas_x) {
+            x = -canvas_x;
+            vx *= -0.5;
+        } else if (x > canvas_x) {
+            x = canvas_x;
+            vx *= -0.5;
+        }
+        if (y < -canvas_y) {
+            y = -canvas_y;
+            vy *= -0.5;
+        } else if (y > canvas_y) {
+            y = canvas_y;
+            vy *= -0.5;
+        }
+
+        if (!show_ui) {
+            const dx = mouse_x - x;
+            const dy = mouse_y - y;
+            const dist = dx*dx + dy*dy;
+
+            if (dist < mouse_r2 && dist > 0) {
+                const influence = mouse_strength / Math.sqrt(dist);
+
+                if (lmb) {
+                    vx -= dx * influence;
+                    vy -= dy * influence;
+                } 
+                if (rmb) {
+                    vx += dx * influence;
+                    vy += dy * influence;
+                }
+            }
+        }
+
+        new_particles[p] = x;
+        new_particles[p+1] = y;
+
+        new_particles[p+2] = vx;
+        new_particles[p+3] = vy;
+
+        new_particles[p+4] = x + vx;
+        new_particles[p+5] = y + vy;
+    }
+
+    const count = num_particles*num_vars;
+
+    for (let i = 0; i < count; i++) {
+        particle_data[i] = new_particles[i];
+    }
+
+    for (let i = 0; i < num_particles; i++) {
+        let j1 = i*num_vars;
+        let j2 = i*render_vars;
+        
+        normalized_particles[j2] = particle_data[j1] * inv_cw;
+        normalized_particles[j2+1] = particle_data[j1+1] * inv_ch;
+    }
+
+    device.queue.writeBuffer(instance_buffer, 0, normalized_particles);
+
+    const encoder = device.createCommandEncoder();
+
+    const pass = encoder.beginRenderPass({
+        colorAttachments: [{
+        view: context.getCurrentTexture().createView(),
+        clearValue: { r: 0, g: 0, b: 0, a: 1 },
+        loadOp: "clear",
+        storeOp: "store",
+    }]
+    });
+
+    pass.setPipeline(pipeline);
+    // pass.setBindGroup(0, bindGroup);
+    pass.setVertexBuffer(0, quad_buffer);
+    pass.setVertexBuffer(1, instance_buffer);
+    pass.draw(4, num_particles, 0, 0);
+
+    pass.end()
+
+    device.queue.submit([encoder.finish()]);
+
+    requestAnimationFrame(main_loop);
+}
+
+requestAnimationFrame(main_loop);
---
+++ WaterSimSetup.js
@@ -0,0 +1,119 @@
+function pixels(value) {
+    return String(value) + "px";
+}
+
+const slider_spacing = 50;
+const top_spacing = 10;
+const label_dy = 5;
+const slider_dx = 10;
+const slider_dy = 10;
+const slider_width = 200;
+
+const particle_count_slider = document.getElementById('particle_count_slider');
+const particle_count_label = document.getElementById('particle_count_label');
+particle_count_slider.min = 500;
+particle_count_slider.max = max_particles;
+particle_count_slider.value = num_particles;
+particle_count_slider.width = pixels(slider_width);
+particle_count_slider.left = pixels(slider_dx);
+particle_count_slider.bottom = pixels(slider_dy);
+particle_count_label.left = pixels(slider_dx + slider_width * 0.5);
+particle_count_label.bottom = pixels(slider_dy + label_dy);
+particle_count_label.innerHTML = particle_count_slider.value;
+
+const influence_radius_slider = document.getElementById('influence_radius_slider');
+const influence_radius_label = document.getElementById('influence_radius_label');
+influence_radius_slider.min = 10 * 1000;
+influence_radius_slider.max = 150 * 1000;
+influence_radius_slider.value = influence_radius * 1000;
+influence_radius_slider.width = pixels(slider_width);
+influence_radius_slider.left = pixels(slider_dx);
+influence_radius_slider.bottom = pixels(top_spacing + slider_dy);
+influence_radius_label.left = pixels(slider_dx + slider_width * 0.5);
+influence_radius_label.bottom = pixels(top_spacing + slider_dy + label_dy);
+influence_radius_label.innerHTML = toFixed(influence_radius_slider.value * 0.001,0);
+
+const gravity_slider = document.getElementById('gravity_slider');
+const gravity_label = document.getElementById('gravity_label');
+gravity_slider.min = 0;
+gravity_slider.max = 1.5 * 1000;
+gravity_slider.value = -gravity * 1000;
+gravity_slider.width = pixels(slider_width);
+gravity_slider.left = pixels(slider_dx);
+gravity_slider.bottom = pixels(top_spacing * 2 + slider_dy);
+gravity_label.left = pixels(slider_dx + slider_width * 0.5);
+gravity_label.bottom = pixels(top_spacing * 2 + slider_dy + label_dy);
+gravity_label.innerHTML = toFixed(gravity_slider.value * 0.001, 3);
+
+const target_density_slider = document.getElementById('target_density_slider');
+const target_density_label = document.getElementById('target_density_label');
+target_density_slider.min = 0;
+target_density_slider.max = 15 * 1000;
+target_density_slider.value = target_density * 1000;
+target_density_slider.width = pixels(slider_width);
+target_density_slider.left = pixels(slider_dx);
+target_density_slider.bottom = pixels(top_spacing * 3 + slider_dy);
+target_density_label.left = pixels(slider_dx + slider_width * 0.5);
+target_density_label.bottom = pixels(top_spacing * 3 + slider_dy + label_dy);
+target_density_label.innerHTML = toFixed(target_density_slider.value * 0.001, 3);
+
+const pressure_multiplier_slider = document.getElementById('pressure_multiplier_slider');
+const pressure_multiplier_label = document.getElementById('pressure_multiplier_label');
+pressure_multiplier_slider.min = 0.001 * 1000; 
+pressure_multiplier_slider.max = 2.5 * 1000;
+pressure_multiplier_slider.value = pressure_multiplier * 1000;
+pressure_multiplier_slider.width = pixels(slider_width);
+pressure_multiplier_slider.left = pixels(slider_dx);
+pressure_multiplier_slider.bottom = pixels(top_spacing * 4 + slider_dy);
+pressure_multiplier_label.left = pixels(slider_dx + slider_width * 0.5);
+pressure_multiplier_label.bottom = pixels(top_spacing * 4 + slider_dy + label_dy);
+pressure_multiplier_label.innerHTML = toFixed(pressure_multiplier_slider.value * 0.001, 3);
+
+const viscosity_multiplier_slider = document.getElementById('viscosity_multiplier_slider');
+const viscosity_multipler_label = document.getElementById('viscosity_multiplier_label');
+viscosity_multiplier_slider.min = 0;
+viscosity_multiplier_slider.max = 1 * 1000;
+viscosity_multiplier_slider.value = viscosity_multipler * 1000;
+viscosity_multiplier_slider.width = pixels(slider_width);
+viscosity_multiplier_slider.left = pixels(slider_dx);
+viscosity_multiplier_slider.bottom = pixels(top_spacing * 5 + slider_dy);
+viscosity_multipler_label.left = pixels(slider_dx + slider_width * 0.5);
+viscosity_multipler_label.bottom = pixels(top_spacing * 5 + slider_dy + label_dy);
+viscosity_multipler_label.innerHTML = toFixed(viscosity_multiplier_slider.value * 0.001, 3);
+
+const surface_tension_slider = document.getElementById('surface_tension_slider');
+const surface_tension_label = document.getElementById('surface_tension_label');
+surface_tension_slider.min = 0;
+surface_tension_slider.max = 10 * 1000;
+surface_tension_slider.value = surface_tension_mp * 1000;
+surface_tension_slider.width = pixels(slider_width);
+surface_tension_slider.left = pixels(slider_dx);
+surface_tension_slider.bottom = pixels(top_spacing * 6 + slider_dy);
+surface_tension_label.left = pixels(slider_dx + slider_width * 0.5);
+surface_tension_label.bottom = pixels(top_spacing * 6 + slider_dy + label_dy);
+surface_tension_label.innerHTML = toFixed(surface_tension_slider.value * 0.001, 3);
+
+const mouse_range_slider = document.getElementById('mouse_range_slider');
+const mouse_range_label = document.getElementById('mouse_range_label');
+mouse_range_slider.min = 20 * 1000;  
+mouse_range_slider.max = 500 * 1000;
+mouse_range_slider.value = mouse_influence_r * 1000;
+mouse_range_slider.width = pixels(slider_width);
+mouse_range_slider.left = pixels(slider_dx);
+mouse_range_slider.bottom = pixels(top_spacing * 7 + slider_dy);
+mouse_range_label.left = pixels(slider_dx + slider_width * 0.5);
+mouse_range_label.bottom = pixels(top_spacing * 7 + slider_dy + label_dy);
+mouse_range_label.innerHTML = toFixed(mouse_range_slider.value * 0.001, 3);
+
+const mouse_strength_slider = document.getElementById('mouse_strength_slider');
+const mouse_strength_label = document.getElementById('mouse_strength_label');
+mouse_strength_slider.min = 0.01 * 1000;
+mouse_strength_slider.max = 3 * 1000;
+mouse_strength_slider.value = mouse_strength * 1000;
+mouse_strength_slider.width = pixels(slider_width);
+mouse_strength_slider.left = pixels(slider_dx);
+mouse_strength_slider.bottom = pixels(top_spacing * 8 + slider_dy);
+mouse_strength_label.left = pixels(slider_dx + slider_width * 0.5);
+mouse_strength_label.bottom = pixels(top_spacing * 8 + slider_dy + label_dy);
+mouse_strength_label.innerHTML = toFIxed(mouse_strength_slider.value * 0.001, 3);
+
